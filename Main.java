32bab2d5d6b16c2a20d6ee36ee60d3bff69cebda
Ksenia-Mesh <97454789+Ksenia-Mesh@users.noreply.github.com>
2023-01-17 11:49:18 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 58;
+        int height = 1,69
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
